ff10eb2071961057ec6a2c9e793c03e03b7d6c1f
erik.r <erik.r.satoritech@gmail.com>
2025-04-28 18:04:47 -0600

    new changes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,7 +14,7 @@ class TaskController extends Controller
 {
     public function getTasks()
     {
-        $tasks = Task::get();
+        $tasks = Task::with('users')->get();
         
         return response()->json([
             'success' => true,
